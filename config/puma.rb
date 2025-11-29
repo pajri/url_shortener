a328ab1671878cef12a6ed6702c4365b8ee11118
pajri <aprilio.pajri@gmail.com>
2025-11-29 10:56:28 +0700

    Refactor Puma configuration for improved clarity and environment handling

--- config/puma.rb
+++ config/puma.rb
@@ -24,18 +24,29 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+
+# config/puma.rb
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+preload_app!
 
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+rackup      DefaultRackup
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+directory app_dir
+
+bind "unix://#{shared_dir}/sockets/puma.sock"
+state_path "#{shared_dir}/pids/puma.state"
+pidfile "#{shared_dir}/pids/puma.pid"
+
+stdout_redirect "#{shared_dir}/log/puma.stdout.log",
+                "#{shared_dir}/log/puma.stderr.log",
+                true
+
+activate_control_app
